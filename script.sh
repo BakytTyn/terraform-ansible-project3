398a04f7e4db260a9e3ba9c38350b7fddc8a8f73
BakytTyn <bakyta.devops@gmail.com>
2024-05-18 00:16:52 +0000

    Grafana

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install Terraform
+sudo wget https://releases.hashicorp.com/terraform/1.8.3/terraform_1.8.3_linux_amd64.zip
+sudo unzip -o /home/ec2-user/terraform-ansible-project3/terraform_1.8.3_linux_amd64.zip
+sudo mv /home/ec2-user/terraform-ansible-project3/terraform /usr/local/bin/
+sudo rm -rf terraform_1.8.3_linux_amd64.zip
+sudo terraform version
+
+# Install Ansible
+sudo yum install ansible -y
+sudo ansible --version
